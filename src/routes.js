23d80120ab786fb825e0ec1f155e07b49984e420
lucasalcantarap <lucaspe0685@gmail.com>
2021-05-15 16:44:30 -0300

    alterado html

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = express.Router()
 
+const basePath = __dirname + "/views"
 //request, e response
-routes.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
+routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
+routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
+routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+
 
 //Joga o routes para fora
 module.exports = routes;
