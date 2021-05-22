398e9d70be603d0ca987b5d9f2cc95613aa06ff0
lucasalcantarap <lucaspe0685@gmail.com>
2021-05-22 12:00:42 -0300

    install ejs, mudou  hml para ejs e setado server

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const routes = express.Router()
+const express = require('express'); //bibliotecA para criar os servidor
+const routes = express.Router() // parte do express que cria roras ou caminhos
 
-const basePath = __dirname + "/views"
-//request, e response
-routes.get('/', (request, response) => response.sendFile(basePath + "/index.html"))
-routes.get('/job', (request, response) => response.sendFile(basePath + "/job.html"))
-routes.get('/job/edit', (request, response) => response.sendFile(basePath + "/job-edit.html"))
-routes.get('/profile', (request, response) => response.sendFile(basePath + "/profile.html"))
+const views = __dirname + "/views/" //trouxe o views oara o diretorio
+
+//req, e res
+//O ejs jasabe que o arquivo esta na pasta views
+routes.get('/', (req, res) => res.render(views + "/index"))
+routes.get('/job', (req, res) => res.render(views + "/job"))
+routes.get('/job/edit', (req, res) => res.render(views + "/job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "/profile"))
 
 
 //Joga o routes para fora
