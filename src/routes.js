1c1eade94787c46c6cffaf7282dec6af953c5c51
lucasalcantarap <lucaspe0685@gmail.com>
2021-05-22 13:00:13 -0300

    template engine profile

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,21 @@ const routes = express.Router() // parte do express que cria roras ou caminhos
 
 const views = __dirname + "/views/" //trouxe o views oara o diretorio
 
+const profile = {
+    name: "Lucas",
+    avatar: "https://avatars.githubusercontent.com/u/50846408?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5, 
+    "vacation-per-year": 4
+}
+
 //req, e res
 //O ejs jasabe que o arquivo esta na pasta views
 routes.get('/', (req, res) => res.render(views + "/index"))
 routes.get('/job', (req, res) => res.render(views + "/job"))
 routes.get('/job/edit', (req, res) => res.render(views + "/job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "/profile"))
+routes.get('/profile', (req, res) => res.render(views + "/profile", { profile }))
 
 
 //Joga o routes para fora
